a0cf3cfc37044e914593e60fce299d02653f0238
djunaskit <id.waskito@gmail.com>
2026-06-03 13:05:44 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
+function calculate(){
 
+let power = parseFloat(
+document.getElementById("power").value
+);
+
+let voltage = parseFloat(
+document.getElementById("voltage").value
+);
+
+let pf = parseFloat(
+document.getElementById("pf").value
+);
+
+let eff = parseFloat(
+document.getElementById("eff").value
+);
+
+let current =
+(power * 1000) /
+(1.732 * voltage * pf * eff);
+
+document.getElementById("result")
+.innerHTML =
+"Current = " +
+current.toFixed(2) +
+" A";
+
+}
